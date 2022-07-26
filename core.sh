a13e1d461f93c09a2adadc704334b92594383bbf
Julien Bernard <julien.bernard@univ-fcomte.fr>
2022-07-26 12:18:24 +0200

    use system-wide xembed

--- core.sh
+++ core.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-build/xembed core.agate core.inc AgateCore
+if which xembed > /dev/null
+then
+  xembed core.agate core.inc AgateCore
+else
+  echo "Install xembed. See https://github.com/agate-lang/xembed."
+fi
